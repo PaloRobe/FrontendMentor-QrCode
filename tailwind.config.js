85561a6c72a87309b58a1fc6ab65b4e4ffa8a4c4
r.palombelli <r.palombelli@eustema.it>
2024-10-02 21:22:16 +0200

    qr-code challenge solution

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+
+  theme: {
+    extend: {
+      fontFamily:{
+        OutFit:["Outfit", "sans-serif"],
+      }
+    },
+    colors:{
+    White: "hsl(0, 0%, 100%)",
+    Slate300:"hsl(212, 45%, 89%)",
+    Slate500:"hsl(216, 15%, 48%)",
+    Slate900:"hsl(218, 44%, 22%)",
+    }
+  },
+  plugins: [],
+}
+
